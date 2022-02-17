079979addf0df9d7823956e37fa0e4f1aca32991
Theger14 <jinhengyap@gmail.com>
2022-02-17 10:08:45 +0000

    refactor: remove web3.js usage

--- src/ethereum/sushiswap-consts.ts
+++ src/ethereum/sushiswap-consts.ts
@@ -1,5 +1,4 @@
 import { utils } from 'ethers';
-import web3 from 'web3';
 import { SushiswapConstsType } from '../types';
 
 export const SushiswapConsts: SushiswapConstsType = {
@@ -8,8 +7,10 @@ export const SushiswapConsts: SushiswapConstsType = {
   ROUTER: '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F',
   FORGE_ID_COMPLEX: utils.formatBytes32String('SushiswapComplex'),
   FORGE_ID_SIMPLE: utils.formatBytes32String('SushiswapSimple'),
-  CODE_HASH: web3.utils.hexToBytes(
-    '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303'
+  CODE_HASH: Array.from(
+    utils.arrayify(
+      '0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303'
+    )
   ),
   PAIR_FACTORY: '0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac',
 };
--- src/ethereum/uniswap-consts.ts
+++ src/ethereum/uniswap-consts.ts
@@ -1,10 +1,11 @@
 import { utils } from 'ethers';
-import web3 from 'web3';
 import { UniswapConstsType } from '../types';
 
 export const UniswapConsts: UniswapConstsType = {
-  CODE_HASH: web3.utils.hexToBytes(
-    '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+  CODE_HASH: Array.from(
+    utils.arrayify(
+      '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+    )
   ),
   PAIR_FACTORY: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
   FORGE_ID: utils.formatBytes32String('UniswapV2'),
