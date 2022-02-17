d3e46d4d1c9211e3d8a362f637ddfd87ffb6caa7
Theger14 <jinhengyap@gmail.com>
2022-02-17 10:08:45 +0000

    refactor: remove web3.js usage

--- src/avalanche/traderjoe-consts.ts
+++ src/avalanche/traderjoe-consts.ts
@@ -1,5 +1,4 @@
 import { utils } from 'ethers';
-import web3 from 'web3';
 import { TraderJoeConstsType } from '../types';
 
 export const TraderJoeConsts: TraderJoeConstsType = {
@@ -7,8 +6,10 @@ export const TraderJoeConsts: TraderJoeConstsType = {
   ROUTER: '0x60aE616a2155Ee3d9A68541Ba4544862310933d4',
   FORGE_ID_COMPLEX: utils.formatBytes32String('TraderJoeComplex'),
   FORGE_ID_SIMPLE: utils.formatBytes32String('TraderJoeSimple'),
-  CODE_HASH: web3.utils.hexToBytes(
-    '0x0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91'
+  CODE_HASH: Array.from(
+    utils.arrayify(
+      '0x0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91'
+    )
   ),
   PAIR_FACTORY: '0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10',
   FORGE_ID_XJOE: utils.formatBytes32String('xJoe'),
