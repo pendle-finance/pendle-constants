714ee8c6b95bf129c56c408a09a0b32654646348
UncleGrandpa925 <vuonglongtoan@gmail.com>
2023-01-20 14:18:58 +0700

    Continue reformatting package

--- src/ethereum-v2/main.ts
+++ src/ethereum-v2/main.ts
@@ -3,7 +3,7 @@ import {EthV2Tokens} from "./tokens";
 import {EthV2Markets} from "./markets";
 
 export const EthV2 = {
-    protocol: EthV2Protocol,
+    ...EthV2Protocol,
+    ...EthV2Markets,
     tokens: EthV2Tokens,
-    markets: EthV2Markets
 }
